dfdab006af42609f3ff4cd3da4051f5561bc5b00
tafiagu <tafiagu@gmail.com>
2024-02-20 11:21:45 +0800

    bugfix: not block in main thread

--- src/ios/VersionChecker.m
+++ src/ios/VersionChecker.m
@@ -5,56 +5,58 @@
 //  Created by 顾九 on 2020/10/14.
 //
 
+
 #import <UIKit/UIKit.h>
 #import "VersionChecker.h"
+
 
 @implementation VersionChecker
 
 
 + (void)check:(CheckBlock) block{
-    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"FirConfig" ofType:@"plist"];
-    NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
-    NSString *appId = [data objectForKey:@"FIR_IOS_APP_ID"];
-    NSString *apiToken = [data objectForKey:@"FIR_API_TOKEN"];
-    
-    NSString *idUrlString = [NSString stringWithFormat:@"http://api.bq04.com/apps/latest/%@?api_token=%@",appId, apiToken];
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:idUrlString]];
-    
-    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-    NSURLSessionTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (!error && data) {
-            NSError *jsonError = nil;
-            id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-            if (!jsonError && [object isKindOfClass:[NSDictionary class]]) {
-                NSString *code = object[@"code"];
-                NSString *errors = object[@"errors"];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"FirConfig" ofType:@"plist"];
+        NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+        NSString *appId = [data objectForKey:@"FIR_IOS_APP_ID"];
+        NSString *apiToken = [data objectForKey:@"FIR_API_TOKEN"];
 
-                if (code && errors) {
-                    block(nil);
-                } else {
-                    NSString *version = object[@"version"];
-                    NSString *build = object[@"build"];
-                    NSString *currentBuild = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+        NSString *idUrlString = [NSString stringWithFormat:@"http://api.bq04.com/apps/latest/%@?api_token=%@",appId, apiToken];
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:idUrlString]];
 
-                    build = [build stringByReplacingOccurrencesOfString:@"." withString:@""];
-                    currentBuild = [currentBuild stringByReplacingOccurrencesOfString:@"." withString:@""];
+        NSURLSessionTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (!error && data) {
+                    NSError *jsonError = nil;
+                    id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                    if (!jsonError && [object isKindOfClass:[NSDictionary class]]) {
+                        NSString *code = object[@"code"];
+                        NSString *errors = object[@"errors"];
 
-                    if ([build integerValue] > [currentBuild integerValue]) {
-                        NSLog(@"FIR － 检测到新版本 v%@(%@) ",version,build);
-                        block(object[@"update_url"]);
-                    } else {
-                        block(nil);
+                        if (code && errors) {
+                            block(nil);
+                        } else {
+                            NSString *version = object[@"version"];
+                            NSString *build = object[@"build"];
+                            NSString *newUrl = object[@"update_url"];
+                            NSString *currentBuild = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+
+                            build = [build stringByReplacingOccurrencesOfString:@"." withString:@""];
+                            currentBuild = [currentBuild stringByReplacingOccurrencesOfString:@"." withString:@""];
+
+                            if ([build integerValue] > [currentBuild integerValue]) {
+                                NSLog(@"FIR － 检测到新版本 v%@(%@), url:%@ ",version,build, newUrl);
+                                block(newUrl);
+                            } else {
+                                block(nil);
+                            }
+                        }
                     }
+                } else {
+                    block(nil);
                 }
-            }
-        } else {
-            block(nil);
-        }
-        dispatch_semaphore_signal(semaphore);
-    }];
-    [dataTask resume];
-    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+            });
+        }];
+        [dataTask resume];
+    });
 }
-
 @end
-
